4024d197a862e893494adf1d042b83fda55540b0
Walter Yang <yandongy@codeaurora.org>
2015-03-16 15:55:19 +0800

    ASoC: wcd: correct the swap plug type detection
    For swap plug type detection, only if it's detected as swap type
    for four consecutive times, it will be set as swap. If there is
    some problem during the detection, the longest mis-operation
    period we can wait for is also four-consecutive-time detection.
    So even if there is some unexpected behavior like HPH PA being
    turned on, the fourth detection won't become the singular decisive
    one.
    
    CRs-Fixed: 806833
    
    Change-Id: Icdb9d8e222b8cd9cb239deddfc637d6b874ad819
    Signed-off-by: Walter Yang <yandongy@codeaurora.org>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -951,6 +951,7 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 	u16 result1, result2;
 	bool wrk_complete = false;
 	int pt_gnd_mic_swap_cnt = 0;
+	int no_gnd_mic_swap_cnt = 0;
 	bool is_pa_on;
 	bool micbias2;
 	bool micbias1;
@@ -996,6 +997,7 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 			if (wcd_check_cross_conn(mbhc)) {
 				plug_type = MBHC_PLUG_TYPE_GND_MIC_SWAP;
 				pt_gnd_mic_swap_cnt++;
+				no_gnd_mic_swap_cnt = 0;
 				if (pt_gnd_mic_swap_cnt <
 						GND_MIC_SWAP_THRESHOLD) {
 					continue;
@@ -1019,13 +1021,14 @@ static void wcd_correct_swch_plug(struct work_struct *work)
 						continue;
 				}
 			} else {
-				pt_gnd_mic_swap_cnt++;
+				no_gnd_mic_swap_cnt++;
+				pt_gnd_mic_swap_cnt = 0;
 				plug_type = MBHC_PLUG_TYPE_HEADSET;
-				if (pt_gnd_mic_swap_cnt <
+				if (no_gnd_mic_swap_cnt <
 						GND_MIC_SWAP_THRESHOLD) {
 					continue;
 				} else {
-					pt_gnd_mic_swap_cnt = 0;
+					no_gnd_mic_swap_cnt = 0;
 				}
 			}
 		}
