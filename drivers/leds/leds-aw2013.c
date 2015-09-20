c24c36879d3d3c1b2f9a91cbd2d96cf22d1f7568
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-09-20 22:36:33 +0700

    leds-aw2013: Disable vdd control for HM2014811
    AW2013 on HM2014811 is powered by "always on" l16 regulator
    
    Change-Id: I45bb93c701ae131cc3842514d8b34287fed29d2a

--- drivers/leds/leds-aw2013.c
+++ drivers/leds/leds-aw2013.c
@@ -41,8 +41,10 @@
 #define AW_LED_RESET_MASK		0x55
 
 #define AW_LED_RESET_DELAY		8
+#ifndef CONFIG_MACH_HM2014811
 #define AW2013_VDD_MIN_UV		2600000
 #define AW2013_VDD_MAX_UV		3300000
+#endif
 #define AW2013_VI2C_MIN_UV		1800000
 #define AW2013_VI2C_MAX_UV		1800000
 
@@ -58,7 +60,9 @@ struct aw2013_led {
 	struct work_struct work;
 	struct workqueue_struct *workqueue;
 	struct mutex lock;
+#ifndef CONFIG_MACH_HM2014811
 	struct regulator *vdd;
+#endif
 	struct regulator *vcc;
 	int num_leds;
 	int id;
@@ -90,12 +94,14 @@ static int aw2013_power_on(struct aw2013_led *led, bool on)
 	int rc;
 
 	if (on) {
+#ifndef CONFIG_MACH_HM2014811
 		rc = regulator_enable(led->vdd);
 		if (rc) {
 			dev_err(&led->client->dev,
 				"Regulator vdd enable failed rc=%d\n", rc);
 			return rc;
 		}
+#endif
 
 		rc = regulator_enable(led->vcc);
 		if (rc) {
@@ -105,12 +111,14 @@ static int aw2013_power_on(struct aw2013_led *led, bool on)
 		}
 		led->poweron = true;
 	} else {
+#ifndef CONFIG_MACH_HM2014811
 		rc = regulator_disable(led->vdd);
 		if (rc) {
 			dev_err(&led->client->dev,
 				"Regulator vdd disable failed rc=%d\n", rc);
 			return rc;
 		}
+#endif
 
 		rc = regulator_disable(led->vcc);
 		if (rc) {
@@ -123,18 +131,22 @@ static int aw2013_power_on(struct aw2013_led *led, bool on)
 	return rc;
 
 fail_enable_reg:
+#ifndef CONFIG_MACH_HM2014811
 	rc = regulator_disable(led->vdd);
 	if (rc)
 		dev_err(&led->client->dev,
 			"Regulator vdd disable failed rc=%d\n", rc);
+#endif
 
 	return rc;
 
 fail_disable_reg:
+#ifndef CONFIG_MACH_HM2014811
 	rc = regulator_enable(led->vdd);
 	if (rc)
 		dev_err(&led->client->dev,
 			"Regulator vdd enable failed rc=%d\n", rc);
+#endif
 
 	return rc;
 }
@@ -144,6 +156,7 @@ static int aw2013_power_init(struct aw2013_led *led, bool on)
 	int rc;
 
 	if (on) {
+#ifndef CONFIG_MACH_HM2014811
 		led->vdd = regulator_get(&led->client->dev, "vdd");
 		if (IS_ERR(led->vdd)) {
 			rc = PTR_ERR(led->vdd);
@@ -162,6 +175,7 @@ static int aw2013_power_init(struct aw2013_led *led, bool on)
 				goto reg_vdd_put;
 			}
 		}
+#endif
 
 		led->vcc = regulator_get(&led->client->dev, "vcc");
 		if (IS_ERR(led->vcc)) {
@@ -181,10 +195,12 @@ static int aw2013_power_init(struct aw2013_led *led, bool on)
 			}
 		}
 	} else {
+#ifndef CONFIG_MACH_HM2014811
 		if (regulator_count_voltages(led->vdd) > 0)
 			regulator_set_voltage(led->vdd, 0, AW2013_VDD_MAX_UV);
 
 		regulator_put(led->vdd);
+#endif
 
 		if (regulator_count_voltages(led->vcc) > 0)
 			regulator_set_voltage(led->vcc, 0, AW2013_VI2C_MAX_UV);
@@ -196,10 +212,12 @@ static int aw2013_power_init(struct aw2013_led *led, bool on)
 reg_vcc_put:
 	regulator_put(led->vcc);
 reg_vdd_set_vtg:
+#ifndef CONFIG_MACH_HM2014811
 	if (regulator_count_voltages(led->vdd) > 0)
 		regulator_set_voltage(led->vdd, 0, AW2013_VDD_MAX_UV);
 reg_vdd_put:
 	regulator_put(led->vdd);
+#endif
 	return rc;
 }
 
